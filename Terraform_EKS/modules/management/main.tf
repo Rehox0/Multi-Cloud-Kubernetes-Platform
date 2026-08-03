ce87f50f28cf3580aafacee62a76684afb078e1d
Łukasz <rehox0@gmail.com>
2026-08-03 15:03:00 +0200

    Deleted unsupported ec2 instance

--- Terraform_EKS/modules/management/main.tf
+++ Terraform_EKS/modules/management/main.tf
@@ -41,12 +41,6 @@ resource "aws_autoscaling_group" "management" {
         launch_template_id = aws_launch_template.management.id
         version            = "$Latest"
       }
-      override {
-        instance_type = "t3.micro"
-      }
-      override {
-        instance_type = "t3a.micro"
-      }
     } 
   } 
 }
