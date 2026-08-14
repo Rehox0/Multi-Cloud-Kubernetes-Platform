282ca3220df49021b469d125389afe0891e1333c
Łukasz <rehox0@gmail.com>
2026-08-14 19:04:30 +0200

    fixing connection control plane -> webhooks

--- Terraform_EKS/modules/iam/irsa.karpenter.tf
+++ Terraform_EKS/modules/iam/irsa.karpenter.tf
@@ -124,3 +124,9 @@ resource "aws_iam_instance_profile" "karpenter_node" {
 
   tags = var.common_tags
 }
+
+resource "aws_eks_access_entry" "karpenter_node" {
+  cluster_name  = var.cluster_name
+  principal_arn = aws_iam_role.karpenter_node.arn
+  type          = "EC2_LINUX"
+} 
