e262948baa107712fe400e988eb31f9a2b46f600
Łukasz <rehox0@gmail.com>
2026-08-26 15:10:17 +0200

    Feat: GHA OIDC

--- Terraform_EKS/modules/iam/oidc.tf
+++ Terraform_EKS/modules/iam/oidc.tf
@@ -5,3 +5,15 @@ resource "aws_iam_openid_connect_provider" "eks" {
 
   tags = var.common_tags
 }
+
+resource "aws_iam_openid_connect_provider" "github_actions" {
+  url = var.gha_oidc_url
+
+  client_id_list = ["sts.amazonaws.com"]
+
+  thumbprint_list = [
+    data.tls_certificate.github_actions.certificates[0].sha1_fingerprint
+  ]
+
+  tags = var.common_tags
+}
